34a746b2b2500f77dfca199c8d6bc52b77325ef0
Zoran Sinnema <zsinnema@gmail.com>
2020-04-08 08:43:20 -0500

    fix: replace 4 spaces by 2

--- applications/frontend/src/types/model.ts
+++ applications/frontend/src/types/model.ts
@@ -2,9 +2,9 @@ import { FeaturedType } from './global'
 
 // Model object
 export interface Model {
-    id: number;
-    name: string;
-    description: string;
-    image: string;
-    type: FeaturedType;
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+  type: FeaturedType;
 }
