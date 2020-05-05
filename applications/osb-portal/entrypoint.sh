fdc85998404fc7a20191d0bef227500c90614643
Filippo Ledda <filippo@metacell.us>
2020-05-05 20:30:52 +0200

    Fix realm address

--- applications/osb-portal/entrypoint.sh
+++ applications/osb-portal/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-sed -i "s/__DOMAIN__/${DOMAIN}/g" /usr/share/nginx/html/keycloak.json
+sed -i "s/__DOMAIN__/${CH_ACCOUNTS_AUTH_DOMAIN}/g" /usr/share/nginx/html/keycloak.json
+sed -i "s/__NAMESPACE__/${CH_ACCOUNTS_REALM}/g" /usr/share/nginx/html/keycloak.json
 
 nginx -g "daemon off;"
