dafd11012976ed632298a766bb278f424a544645
Zoran Sinnema <zsinnema@gmail.com>
2020-05-06 15:54:55 -0500

    fix: check earlier if user is logged in & use web-client client from realm

--- applications/osb-portal/src/index.tsx
+++ applications/osb-portal/src/index.tsx
@@ -5,9 +5,27 @@ import { App } from './components/index';
 import store from './store/store';
 import { Provider } from 'react-redux';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('main')
-);
+import Keycloak from 'keycloak-js';
+import { userLogin } from './store/actions/user';
+
+export const keycloak = Keycloak('/keycloak.json');
+keycloak.init({
+  onLoad: 'check-sso',
+  silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html'
+}).then(async (authorized: boolean) => {
+  if (authorized) {
+    const userInfo: any = await keycloak.loadUserInfo();
+    store.dispatch(userLogin({
+        id: userInfo.sub,
+        firstName: userInfo.given_name,
+        lastName: userInfo.family_name,
+        emailAddress: userInfo.email
+      }));
+  }
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('main')
+  );
+});
