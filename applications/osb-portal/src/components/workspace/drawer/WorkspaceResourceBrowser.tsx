52af7db6eadf07f2b5e26bd28f2d829d3a922c4c
Zoran Sinnema <zsinnema@gmail.com>
2020-09-29 16:30:38 +0200

    #65 chore: implemented rest api call open resource & only add onclick for active resources

--- applications/osb-portal/src/components/workspace/drawer/WorkspaceResourceBrowser.tsx
+++ applications/osb-portal/src/components/workspace/drawer/WorkspaceResourceBrowser.tsx
@@ -6,10 +6,8 @@ import Typography from '@material-ui/core/Typography';
 import TreeView from "@material-ui/lab/TreeView";
 import TreeItem from "@material-ui/lab/TreeItem";
 import ArrowDownIcon from "@material-ui/icons/ArrowDropDown";
-import { ResourceStatus, Workspace, WorkspaceResource, ResourceType } from "../../../types/workspace";
-
-
-import WorkspaceResourceService from '../../../service/WorkspaceResourceService';
+import { ResourceStatus, Workspace, WorkspaceResource } from "../../../types/workspace";
+import WorkspaceResourceService from "../../../service/WorkspaceResourceService";
 
 import {
   FileLinkIcon,
@@ -20,14 +18,24 @@ import {
 
 const openFileResource = (resource: WorkspaceResource) => (e: any) => {
   const fileName = resource.location.slice(resource.location.lastIndexOf("/")+1);
-  const iFrame: HTMLIFrameElement = document.getElementById("workspace-frame") as HTMLIFrameElement;
-  iFrame.contentWindow.postMessage(fileName, '*');
+  const r = WorkspaceResourceService.workspacesControllerWorkspaceResourceOpen(resource.id).then(() => {
+    const iFrame: HTMLIFrameElement = document.getElementById("workspace-frame") as HTMLIFrameElement;
+    iFrame.contentWindow.postMessage(fileName, '*');
+  }).catch(() => {
+    alert("Error open resource, ResourceOpen function failed!");
+  });
 }
 
 const OSBTreeItem = (props: { resource: WorkspaceResource }) => {
   const { resource } = props;
+  const canOpenFile: boolean = resource.status == ResourceStatus.available;
   return (
-    <Box display="flex" alignItems="center" justifyContent="space-between" onClick={openFileResource(resource)} >
+    <Box 
+      display="flex" 
+      alignItems="center" 
+      justifyContent="space-between" 
+      onClick={canOpenFile ? openFileResource(resource) : undefined}
+    >
       {resource.type.application === null ? <FolderIcon /> : ""}
       <Typography style={{ opacity: resource.status === ResourceStatus.pending ? 0.3 : 1 }}>{resource.name}</Typography>
       {resource.status === ResourceStatus.pending ? <LoadingIcon /> : null}
