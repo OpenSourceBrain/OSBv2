1eac2c0a6a25a97f07ad7c2bb417c35a03e344b1
Zoran Sinnema <zsinnema@gmail.com>
2020-09-29 16:43:21 +0200

    #65 chore: fix linter issues

--- applications/osb-portal/src/components/workspace/drawer/WorkspaceResourceBrowser.tsx
+++ applications/osb-portal/src/components/workspace/drawer/WorkspaceResourceBrowser.tsx
@@ -17,7 +17,7 @@ import {
 
 
 const openFileResource = (resource: WorkspaceResource) => (e: any) => {
-  const fileName = resource.location.slice(resource.location.lastIndexOf("/")+1);
+  const fileName = resource.location.slice(resource.location.lastIndexOf("/") + 1);
   const r = WorkspaceResourceService.workspacesControllerWorkspaceResourceOpen(resource.id).then(() => {
     const iFrame: HTMLIFrameElement = document.getElementById("workspace-frame") as HTMLIFrameElement;
     iFrame.contentWindow.postMessage(fileName, '*');
@@ -28,12 +28,12 @@ const openFileResource = (resource: WorkspaceResource) => (e: any) => {
 
 const OSBTreeItem = (props: { resource: WorkspaceResource }) => {
   const { resource } = props;
-  const canOpenFile: boolean = resource.status == ResourceStatus.available;
+  const canOpenFile: boolean = resource.status === ResourceStatus.available;
   return (
-    <Box 
-      display="flex" 
-      alignItems="center" 
-      justifyContent="space-between" 
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
       onClick={canOpenFile ? openFileResource(resource) : undefined}
     >
       {resource.type.application === null ? <FolderIcon /> : ""}
