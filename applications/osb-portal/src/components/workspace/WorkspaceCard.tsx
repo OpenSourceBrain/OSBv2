dd0b987ddbd6bbb8151ec7393c5f87929a81f32d
Vidhya Longani <vidhya.longani@kiwitech.com>
2020-10-05 15:39:21 +0530

    #53 Overall responsive styling

--- applications/osb-portal/src/components/workspace/WorkspaceCard.tsx
+++ applications/osb-portal/src/components/workspace/WorkspaceCard.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
+import Box from "@material-ui/core/Box";
 import Link from "@material-ui/core/Link";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
@@ -36,12 +37,11 @@ const useStyles = makeStyles((theme) => ({
   imageContainer: {
     display: "flex",
     justifyContent: "center",
-    height: 150,
-    overflow: "hidden"
+    overflow: "hidden",
   },
   image: {
-    width: "100%"
-  }
+    width: "100%",
+  },
 }));
 
 export const WorkspaceCard = (props: Props) => {
@@ -54,25 +54,30 @@ export const WorkspaceCard = (props: Props) => {
         <Icons.InfoIcon className={classes.icon} />
       </CardActions>
 
-      <div className={classes.imageContainer}>
+      <Box className={classes.imageContainer}>
         <Link href={`/workspace/${workspace.id}`} color="inherit">
           {!workspace.thumbnail ? (
             <FolderIcon className={classes.imageIcon} />
           ) : (
-              <img src={workspace.thumbnail} className={classes.image} title={openTitle} alt={openTitle} />
-            )}
+            <img
+              src={workspace.thumbnail}
+              className={classes.image}
+              title={openTitle}
+              alt={openTitle}
+            />
+          )}
         </Link>
-      </div>
+      </Box>
 
       <CardContent>
-
         <Link href={`/workspace/${workspace.id}`} color="inherit">
           <Typography component="h2" variant="h5">
             {workspace.name}
           </Typography>
         </Link>
         <Typography variant="caption">
-          {workspace.lastOpen.type.application.name}, {formatDate(workspace.timestampUpdated)}
+          {workspace.lastOpen.type.application.name},{" "}
+          {formatDate(workspace.timestampUpdated)}
         </Typography>
       </CardContent>
     </Card>
