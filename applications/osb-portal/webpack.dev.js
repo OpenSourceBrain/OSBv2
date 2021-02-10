d3e7213965d0a1bba6530bce6cd67501aa2c038c
Filippo Ledda <filippo@metacell.us>
2021-02-10 19:54:39 +0100

    #91 fix osb portal image to work on different workspaces

--- applications/osb-portal/webpack.dev.js
+++ applications/osb-portal/webpack.dev.js
@@ -8,7 +8,7 @@ const contentbase = path.join(__dirname, 'public')
 PORT = 3000;
 
 var proxyTarget = 'https://__APP_NAME__/'
-if (process.env.USE_MOCKS){
+if (process.env.USE_MOCKS) {
   console.log('Using mocks')
   proxyTarget = `http://localhost:${PORT}/api-mocks`
 }
@@ -19,34 +19,48 @@ module.exports = env => {
   const osbDomain = env && env.DOMAIN ? env.DOMAIN : 'osb.local';
   const replaceHost = (uri, appName) => uri.replace("__APP_NAME__", appName + '.' + osbDomain);
   return merge(
-  {
-  mode: 'development',
-  devtool: 'eval-source-map',
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
-    publicPath: '/',
-    filename: path.join('js', 'bundle.js'),
-    compress: true,
-    https: true,
-    disableHostCheck: true,
-    historyApiFallback: true,
-    proxy : {
-      '/api/workspaces': {
-        target : replaceHost( proxyTarget, 'workspaces'),
-        secure : false,
-        changeOrigin: true,
-        pathRewrite: {'^/api/workspaces' : ''}
+    {
+      mode: 'development',
+      devtool: 'eval-source-map',
+      devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        publicPath: '/',
+        filename: path.join('js', 'bundle.js'),
+        compress: true,
+        https: true,
+        disableHostCheck: true,
+        historyApiFallback: true,
+        proxy: {
+          '/api/workspaces': {
+            target: replaceHost(proxyTarget, 'workspaces'),
+            secure: false,
+            changeOrigin: true,
+            pathRewrite: { '^/api/workspaces': '' }
+          },
+          '/workspaces/': {
+            target: replaceHost(proxyTarget, 'workspaces'),
+            secure: false,
+            changeOrigin: true,
+          }
+        },
+        port: PORT,
+
       },
-      '/workspaces/': {
-        target : replaceHost( proxyTarget, 'workspaces'),
-        secure : false,
-        changeOrigin: true,
-      }
-    },
-    port: PORT
-  },
 
+      plugins: [
+        new CopyPlugin([
+          {
+            from: './src/assets-parametrized', contentbase,
+            transform(content, path) {
+              return setEnv(content);
+            }
+          },
+          {
+            from: './src/assets', contentbase,
+          },
 
+        ]),
+      ],
 
-}, common(env))
+    }, common(env))
 };
