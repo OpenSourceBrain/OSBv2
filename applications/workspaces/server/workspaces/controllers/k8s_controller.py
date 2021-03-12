3c3ec8e393ec2cce2b1910f8a4a1d941c76d8fee
Filippo Ledda <filippo@metacell.us>
2021-03-12 15:57:01 +0100

    #123 Added liveness/readiness probes to workspace manager

---
+++ applications/workspaces/server/workspaces/controllers/k8s_controller.py
@@ -0,0 +1,24 @@
+from workspaces.service.events import test_kafka_running
+
+
+
+
+def live():  # noqa: E501
+    """Test if application is healthy
+     # noqa: E501
+    :rtype: str
+    """
+    if test_kafka_running():
+        return 'I\'m alive!'
+    else:
+        raise Exception("Live check failed")
+
+def ready():  # noqa: E501
+    """Test if application is healthy
+     # noqa: E501
+    :rtype: str
+    """
+    if test_kafka_running():
+        return 'I\'m ready!'
+    else:
+        raise Exception("Readiness check failed")
