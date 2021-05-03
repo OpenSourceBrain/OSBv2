6a78e1b27dd8dfbb8ab8dfea806c57f28969d5f8
Zoran Sinnema <zsinnema@gmail.com>
2021-05-03 12:44:12 +0200

    #157 chore: use helm variables for secret workspaces

--- applications/workspaces/server/workspaces/controllers/osbrepository_controller.py
+++ applications/workspaces/server/workspaces/controllers/osbrepository_controller.py
@@ -7,23 +7,29 @@ from workspaces.views.api.rest_api_views import OsbrepositoryView
 
 def get_contexts(uri=None, repository_type=None, **kwargs):
     # get the branches of the repository
-    contexts = repository_service.get_contexts(uri, repository_type)
-    return contexts, 200
+    try:
+        contexts = repository_service.get_contexts(uri, repository_type)
+        return contexts, 200
+    except Exception as e:
+        return str(e), 500
 
 
 def get(id_=None, context=None, **kwargs):
     # get the repository
-    osbrepository_ext = OSBRepositoryRepository().get(id=id_)
-    if osbrepository_ext is None:
-        return f"OSBRepository with id {id_} not found.", 404
+    try:
+        osbrepository_ext = OSBRepositoryRepository().get(id=id_)
+        if osbrepository_ext is None:
+            return f"OSBRepository with id {id_} not found.", 404
 
-    osbrepository_ext.context_resources = repository_service.get_resources(
-        osbrepository=osbrepository_ext,
-        context=context)  # use context to get the files
-    osbrepository_ext.contexts = repository_service.get_contexts(
-        repository_type=osbrepository_ext.repository_type,
-        uri=osbrepository_ext.uri)
-    osbrepository_ext.description = repository_service.get_description(
-        osbrepository=osbrepository_ext,
-        context=context)  # use context to get the files
-    return row2dict(osbrepository_ext), 200
+        osbrepository_ext.context_resources = repository_service.get_resources(
+            osbrepository=osbrepository_ext,
+            context=context)  # use context to get the files
+        osbrepository_ext.contexts = repository_service.get_contexts(
+            repository_type=osbrepository_ext.repository_type,
+            uri=osbrepository_ext.uri)
+        osbrepository_ext.description = repository_service.get_description(
+            osbrepository=osbrepository_ext,
+            context=context)  # use context to get the files
+        return row2dict(osbrepository_ext), 200
+    except Exception as e:
+        return str(e), 500
--- applications/workspaces/server/workspaces/service/osbrepository/adapters/githubadapter.py
+++ applications/workspaces/server/workspaces/service/osbrepository/adapters/githubadapter.py
@@ -1,3 +1,4 @@
+import base64
 import requests
 import workspaces.service.etlservice as etlservice
 
@@ -8,8 +9,12 @@ from workspaces.models import RepositoryResourceNode, GITRepositoryResource
 from .utils import add_to_tree
 
 
-GITHUB_USER=get_secret("workspaces", "github-user")
-GITHUB_TOKEN=get_secret("workspaces", "github-token")
+GITHUB_USER = get_secret("workspaces", "github-user")
+GITHUB_USER = GITHUB_USER if GITHUB_USER != "none" else None
+GITHUB_TOKEN = get_secret("workspaces", "github-token")
+GITHUB_TOKEN = GITHUB_TOKEN if GITHUB_TOKEN != "none" else None
+
+logger.info("GitHub user:%s, token:%s.", GITHUB_USER, GITHUB_TOKEN)
 
 
 def _clean_url_and_end_with_slash(url):
@@ -26,14 +31,25 @@ class GitHubAdapter:
         self.api_url = _clean_url_and_end_with_slash(uri.replace("https://github.com/","https://api.github.com/repos/"))
         self.download_base_url = _clean_url_and_end_with_slash(uri.replace("https://github.com/","https://raw.githubusercontent.com/"))
 
+    def get_json(self, uri):
+        r = requests.get(uri, auth=(GITHUB_USER, GITHUB_TOKEN))
+        if r.status_code == 200:
+            return r.json()
+        elif r.status_code == 403:
+            # rate limit hit
+            error = r.json()
+            logger.info(error)
+            raise Exception(f"GitHub adapter error: {error['message']}")
+        else:
+            raise Exception(f"Failed getting GitHub content, url: {uri}, status code: {r.status_code}")
+
     def get_contexts(self):
-        branches = requests.get(self.api_url + "branches", auth=(GITHUB_USER, GITHUB_TOKEN)).json()
-        tags = requests.get(self.api_url + "tags", auth=(GITHUB_USER, GITHUB_TOKEN)).json()
+        branches = self.get_json(self.api_url + "branches")
+        tags = self.get_json(self.api_url + "tags")
         return list([context["name"] for context in branches + tags])
 
     def get_resources(self, context):
-        repo_files = self.api_url + "git" + "/" + "trees" + "/" + context + "?recursive=1"
-        contents = requests.get(repo_files, auth=(GITHUB_USER, GITHUB_TOKEN)).json()
+        contents = self.get_json(f"{self.api_url}git/trees/{context}?recursive=1")
 
         tree = RepositoryResourceNode(resource=GITRepositoryResource(name="/"), children=[])
         for git_obj in contents["tree"]:
@@ -58,13 +74,12 @@ class GitHubAdapter:
         # get the latest version of the file specified in the download url
         context = self._get_context_from_url(download_url)
         path = self._get_path_from_url(download_url)
-        file = requests.get(f"{self.api_url}contents/{path}?ref={context}", auth=(GITHUB_USER, GITHUB_TOKEN)).json()
+        file = self.get_json(f"{self.api_url}contents/{path}?ref={context}")
         return file["sha"]
 
     def get_description(self, context):
         try:
-            url = f"{self.api_url}readme?ref={context}"
-            result = requests.get(url, auth=(GITHUB_USER, GITHUB_TOKEN)).json()
+            result = self.get_json(f"{self.api_url}readme?ref={context}")
             description = base64.b64decode(result["content"]).decode('utf-8')
             return description
         except Exception as e:
