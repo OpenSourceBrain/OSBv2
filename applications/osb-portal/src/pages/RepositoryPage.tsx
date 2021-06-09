9cb3ce9eda7bc338436ab547236bc5e8139e9957
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2021-06-09 15:16:01 -0400

    #175 fix import for redux to work

--- applications/osb-portal/src/pages/RepositoryPage.tsx
+++ applications/osb-portal/src/pages/RepositoryPage.tsx
@@ -22,7 +22,7 @@ import RepositoryResourceBrowser from "../components/repository/RepositoryResour
 import OSBDialog from '../components/common/OSBDialog';
 import WorkspaceEditor from "../components/workspace/WorkspaceEditor";
 import OSBChipList from "../components/common/OSBChipList";
-import NewWorkspaceAskUser from "../components/workspace/NewWorkspaceAskUser";
+import { NewWorkspaceAskUser } from "../components";
 import { ExistingWorkspaceEditor, ExistingWorkspaceEditorActions } from "../components/workspace/ExistingWorkspaceEditor";
 import { Workspace } from "../types/workspace";
 import WorkspaceService from "../service/WorkspaceService";
