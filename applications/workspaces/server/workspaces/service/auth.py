1c2d1715e206871e10cc64e541112eb2acbf2741
Filippo Ledda <filippo@metacell.us>
2021-06-09 20:38:21 +0200

    #175 snakk fix, improve resilience of workspaces microservice

--- applications/workspaces/server/workspaces/service/auth.py
+++ applications/workspaces/server/workspaces/service/auth.py
@@ -7,17 +7,17 @@ from cloudharness.auth import AuthClient
 # Reuse AuthClient to benefit from valid token
 # AuthClient will try to refresh automatically once token expired
 
-auth_client = None
+_auth_client = None
 keycloak_running = False
 nap_time = 1
-def _init_auth_client():
-    while not keycloak_running:
+
+
+def get_auth_client():
+    global _auth_client, keycloak_running
+    if not _auth_client:
         try:
-            auth_client = AuthClient()
+            _auth_client = AuthClient()
             keycloak_running = True
         except Exception as e:
-            log.error(f"Keycloak not running? Going for a {nap_time} seconds power nap and will try again later")
-            time.sleep(nap_time)  # sleep 30 seconds and try again
-
-p = Process(target=_init_auth_client)
-p.start()
+            raise Exception("Keycloak not available") from e
+    return _auth_client
