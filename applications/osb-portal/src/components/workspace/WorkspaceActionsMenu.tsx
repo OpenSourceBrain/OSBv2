970bf22756ee89418a8de1030ebc84dfdca493a6
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2021-06-10 13:14:47 -0400

    #169 refactor workspace actions menu 2

--- applications/osb-portal/src/components/workspace/WorkspaceActionsMenu.tsx
+++ applications/osb-portal/src/components/workspace/WorkspaceActionsMenu.tsx
@@ -1,46 +1,59 @@
 import * as React from "react";
+
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import NestedMenuItem from "material-ui-nested-menu-item";
+import { IconButton } from "@material-ui/core";
+import * as Icons from "../icons";
 
 import { OSBApplications, Workspace } from "../../types/workspace";
 import OSBDialog from "../common/OSBDialog";
 import WorkspaceEdit from "./WorkspaceEditor";
+import { canEditWorkspace } from '../../service/UserService';
+import { UserInfo } from "../../types/user";
 
 
 interface WorkspaceActionsMenuProps {
-    canEdit: boolean;
     workspace: Workspace;
-    anchorEl: any;
-    handleCloseMenu: () => void;
     updateWorkspace?: (ws: Workspace) => null;
     deleteWorkspace?: (wsId: number) => null;
     refreshWorkspaces?: () => null;
+    user?: UserInfo;
 }
 
 
 export default (props: WorkspaceActionsMenuProps) => {
     const [editWorkspaceOpen, setEditWorkspaceOpen] = React.useState(false);
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const canEdit = canEditWorkspace(props.user, props.workspace);
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    }
+
+    const handleCloseMenu = () => {
+        setAnchorEl(null);
+    }
 
     const handleEditWorkspace = () => {
         setEditWorkspaceOpen(true);
-        props.handleCloseMenu();
+        handleCloseMenu();
     }
 
     const handleDeleteWorkspace = () => {
         props.deleteWorkspace(props.workspace.id);
-        props.handleCloseMenu();
+        handleCloseMenu();
         props.refreshWorkspaces();
     }
 
     const handlePublicWorkspace = () => {
         props.updateWorkspace({...props.workspace, publicable: true});
-        props.handleCloseMenu();
+        handleCloseMenu();
     }
 
     const handlePrivateWorkspace = () => {
         props.updateWorkspace({ ...props.workspace, publicable: false });
-        props.handleCloseMenu();
+        handleCloseMenu();
     }
 
     const handleOpenWorkspace = () => {
@@ -62,17 +75,20 @@ export default (props: WorkspaceActionsMenuProps) => {
 
     return (
         <>
+            <IconButton size="small" onClick={handleClick}>
+                <Icons.Dots style={{fontSize: "1rem" }}/>
+            </IconButton>
             <Menu
                 id="simple-menu"
-                anchorEl={props.anchorEl}
+                anchorEl={anchorEl}
                 keepMounted={true}
-                open={Boolean(props.anchorEl)}
-                onClose={props.handleCloseMenu}
+                open={Boolean(anchorEl)}
+                onClose={handleCloseMenu}
             >
-                {props.canEdit && <MenuItem onClick={handleEditWorkspace}>Edit</MenuItem>}
-                {props.canEdit && <MenuItem onClick={handleDeleteWorkspace}>Delete</MenuItem>}
-                {props.canEdit && !props.workspace.publicable && <MenuItem onClick={handlePublicWorkspace}>Make public</MenuItem>}
-                {props.canEdit && props.workspace.publicable && <MenuItem onClick={handlePrivateWorkspace}>Make private</MenuItem>}
+                {canEdit && <MenuItem onClick={handleEditWorkspace}>Edit</MenuItem>}
+                {canEdit && <MenuItem onClick={handleDeleteWorkspace}>Delete</MenuItem>}
+                {canEdit && !props.workspace.publicable && <MenuItem onClick={handlePublicWorkspace}>Make public</MenuItem>}
+                {canEdit && props.workspace.publicable && <MenuItem onClick={handlePrivateWorkspace}>Make private</MenuItem>}
                 <MenuItem onClick={handleOpenWorkspace}>Open workspace</MenuItem>
                 <NestedMenuItem
                 label="Open with..."
