e0d3d2d4df63fd428cee1a6fe21bc1c31a4cadbb
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2022-01-18 12:46:23 +0000

    feat: add about-dialog slice, reducers, actions

---
+++ applications/osb-portal/src/store/reducers/aboutdialog.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export const initialState: boolean = false;
+
+export const aboutDialogSlice = createSlice({
+    name: 'aboutDialog',
+    initialState,
+    reducers: {
+        openDialog(state) {
+            state = true;
+            return state;
+        },
+        closeDialog(state) {
+            state = false;
+            return state;
+        },
+    }
+});
+
+export const AboutDialogActions = aboutDialogSlice.actions;
+
+export default aboutDialogSlice.reducer;
