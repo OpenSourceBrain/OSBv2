94b179ef3c9f55dda6cc034776f7cc40d79efdee
Filippo Ledda <filippo@metacell.us>
2022-03-01 15:07:08 +0100

    Fix Error handler

--- applications/osb-portal/src/service/ErrorHandleService.tsx
+++ applications/osb-portal/src/service/ErrorHandleService.tsx
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/react';
 
 export async function initErrorHandler(appName: string) {
 
-    const commonUrl = window.location.host.replace('www', 'common') + '/api/sentry/getdsn/' + appName;
+    const commonUrl = window.location.host + '/proxy/common/api/sentry/getdsn/' + appName;
     fetch(commonUrl)
         .then(response => response.json(), error => console.error("Cannot connect to common service"))
         .then(sentryDSN => {
@@ -10,5 +10,5 @@ export async function initErrorHandler(appName: string) {
                 Sentry.init({ dsn: sentryDSN.dsn })
             }
 
-        }, error => console.error("Cannot connect to error monitor"))
+        }, error => console.error("Cannot connect to error monitor", error))
 }
