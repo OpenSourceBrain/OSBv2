2d4a13df61fd49da00c8f92a3fa19476ec168edc
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2022-05-19 17:38:23 +0100

    squash: add user to page

--- applications/backoffice/src/pages/HomePage.tsx
+++ applications/backoffice/src/pages/HomePage.tsx
@@ -1,8 +1,12 @@
 import * as React from "react";
 
+import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import Typography from '@mui/material/Typography';
 
 import makeStyles from '@mui/styles/makeStyles';
+
+import { getUsers } from  "../service/UserService";
 
 
 
@@ -16,10 +20,33 @@ const useStyles = makeStyles((theme) => ({
 
 export default (props: any) => {
   const classes = useStyles();
+  const [ users, setUsers ] = React.useState<any>(null);
+
+  const fetchUsers = () => {
+    getUsers().then((u) => setUsers(u.users));
+  };
+
+  React.useEffect(() => {
+    fetchUsers();
+  }, [ ]);
+
+  if (!users) {
+    return null;
+  }
 
   return <>
     <Box p={1}>
-     Home
+      {users && users.map((user) => (
+        <Grid
+          container={true}
+          className="row"
+          spacing={0}
+          key={user.username}
+        >
+          < Typography component="strong" >
+            {user.username}
+          </Typography>
+        </Grid>))}
     </Box>
   </>
 };
