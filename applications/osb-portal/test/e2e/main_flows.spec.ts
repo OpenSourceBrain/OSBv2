38b557d38c068001b3f5e111bd7c95ece742a30c
Simão sá <simaosa@Simaos-MacBook-Pro.local>
2022-09-21 15:09:37 +0100

    test step for creating a workspace and deleting the created workspace

--- applications/osb-portal/test/e2e/main_flows.spec.ts
+++ applications/osb-portal/test/e2e/main_flows.spec.ts
@@ -83,6 +83,65 @@ describe("OSB v2 Smoke Tests", () => {
       expect(privateWorkspaces_beforeadding).toBe(0);
     });
 
+    test("Create workspace", async () => {
+      console.log("Creating workspace");
+
+      await page.click(selectors.CREATE_WORKSPACE_SELECTOR);
+      await page.waitForSelector(selectors.WORKSPACE_CREATION_BOX_SELECTOR);
+      await page.type(
+        selectors.WORKSPACE_NAME_SELECTOR,
+        "Smoke Test Workspace"
+      );
+      await page.type(selectors.WORKSPACE_TAGS_SELECTOR, "Test");
+      await page.keyboard.press("Enter");
+      await page.type(
+        selectors.WORKSPACE_DESCRIPTION_SELECTOR,
+        "Workspace created by the Automated Smoke tests"
+      );
+      await page.waitForTimeout(ONE_SECOND);
+
+      await page.waitForTimeout(ONE_SECOND);
+
+      await page.click('#create-a-new-workspace-button')
+      await page.waitForSelector(selectors.SMOKE_TEST_WORKSPACE_SELECTOR);
+      await page.waitForSelector(selectors.YOUR_WORKSPACES_SELECTOR);
+
+      const privateWorkspaces = await page.evaluate(
+        () =>
+          document.querySelectorAll(
+            'div[class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-6 MuiGrid-grid-sm-4 MuiGrid-grid-md-6 MuiGrid-grid-lg-4 MuiGrid-grid-xl-3"]'
+          ).length
+      );
+      expect(privateWorkspaces).toBe(1);
+    });
+
+
+    test("Delete created workspace", async () => {
+      console.log("Deleting created workspace");
+
+      await page.waitForTimeout(ONE_SECOND);
+      await page.click(selectors.OSB_LOGO_SELECTOR);
+      await page.waitForSelector(selectors.SMOKE_TEST_WORKSPACE_SELECTOR);
+      await page.click(selectors.WORKSPACE_OPTIONS_SELECTOR);
+      await page.waitForSelector(selectors.WORKSPACE_OPTIONS_LIST_SELECTOR);
+      await page.evaluate(() => {
+        let map = document.getElementsByClassName(
+          "MuiButtonBase-root MuiListItem-root MuiMenuItem-root MuiMenuItem-gutters MuiListItem-gutters MuiListItem-button"
+        ) as HTMLCollectionOf<HTMLElement>;
+        for (let i = 0; i < 5; i++) {
+          map[i].textContent == "Delete" && map[i].click();
+        }
+      });
+      await page.waitForTimeout(ONE_SECOND);
+      await page.waitForSelector(selectors.YOUR_WORKSPACES_SELECTOR);
+      const privateWorkspaces = await page.evaluate(
+        () =>
+          document.querySelectorAll(
+            'div[class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-6 MuiGrid-grid-sm-4 MuiGrid-grid-md-6 MuiGrid-grid-lg-4 MuiGrid-grid-xl-3"]'
+          ).length
+      );
+      expect(privateWorkspaces).toBe(0);
+    });
 
 
     
