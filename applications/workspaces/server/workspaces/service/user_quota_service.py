a8ae245a423eb5477d120c988efabfe49aec269f
Filippo Ledda <filippo@metacell.us>
2022-11-29 15:49:33 +0100

    #248 ws quotas small fixes

--- applications/workspaces/server/workspaces/service/user_quota_service.py
+++ applications/workspaces/server/workspaces/service/user_quota_service.py
@@ -2,13 +2,13 @@ from cloudharness.applications import get_configuration
 from cloudharness.auth.quota import get_user_quotas
 
 
-def get_pvc_size(application_config, user_id):
+def get_pvc_size(user_id):
     application_config = get_configuration('workspaces')
     user_quotas = get_user_quotas(application_config, user_id=user_id)
     return f"{user_quotas.get('quota-ws-storage-max', '0.1')}Gi"
 
 
-def get_max_workspaces_for_user(application_config, user_id):
+def get_max_workspaces_for_user(user_id) -> int:
     application_config = get_configuration('workspaces')
     user_quotas = get_user_quotas(application_config, user_id=user_id)
-    return user_quotas.get('quota-ws-max', '1')
+    return int(user_quotas.get('quota-ws-max', '1'))
