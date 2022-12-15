8c228b1a0a0a5c4d580dc58d85d89be4acab568a
Aiga115 <aiguljhyldyzbekkyzy@gmail.com>
2022-12-15 12:50:28 +0100

    #628 styles modified and added

--- applications/osb-portal/src/components/common/AddRepoDialog.tsx
+++ applications/osb-portal/src/components/common/AddRepoDialog.tsx
@@ -7,67 +7,122 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
-import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import Autocomplete from '@mui/material/Autocomplete';
+import Chip from '@mui/material/Chip';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
 
 //icons
 import CloseIcon from '@mui/icons-material/Close';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 //style
-import { makeStyles } from "@mui/styles";
-import { bgDarker, secondaryColor, checkBoxColor, bgLight } from '../../theme'
+import { styled } from "@mui/system";
+import { bgDarker, secondaryColor, checkBoxColor, bgLight, badgeBgLight, paragraph, bgDarkest, bgLightest, bgInputs } from '../../theme'
 
 
-const useStyles = makeStyles((theme) => ({
-    dialog: {
+const RepoDialog = styled(Dialog)(({ theme }) => ({
+    "& .MuiPaper-root": {
         padding: 0,
         backgroundColor: bgDarker,
         backgroundImage: 'unset',
         borderRadius: '2px',
-
-        "& .MuiDialogContent-root": {
+    },
+    "& .MuiDialogContent-root": {
+        padding: '0.75rem 1rem',
+        display: 'grid',
+        gap: '12px'
+    },
+    "& .MuiDialogTitle-root": {
+        padding: '0.75rem 1rem',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        borderBottom: `0.0625rem solid ${bgLight}`,
+        '& .MuiTypography-root': {
+            color: secondaryColor,
+            fontWeight: 700
+        },
+        '& .MuiButtonBase-root': {
+            alignSelf: 'end',
             padding: 0,
-        },
-        "& .MuiDialogTitle-root": {
-            padding: '0.75rem 1rem',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            borderBottom: `0.0625rem solid ${bgLight}`,
-            '& .MuiTypography-root': {
-                color: secondaryColor,
-                fontWeight: 700
+            '& .MuiSvgIcon-root': {
+                marginBottom: 0,
+                fill: checkBoxColor
             },
-            '& .MuiButtonBase-root': {
-                alignSelf: 'end',
-                padding: 0,
-                '& .MuiSvgIcon-root': {
-                    marginBottom: 0,
-                    fill: checkBoxColor
-                },
-                '&:hover': {
-                    backgroundColor: 'transparent'
-                }
+            '&:hover': {
+                backgroundColor: 'transparent'
             }
-        },
-        "&. MuiDialogActions-root": {
-            padding: '1rem',
         }
     },
-    paper: {
-        backgroundColor: bgDarker,
-        backgroundImage: 'unset',
+    "& .MuiDialogActions-root": {
+        padding: '1rem',
+        background: bgLightest,
+        boxShadow: '0px -5px 30px rgba(0, 0, 0, 0.25)',
+        border: `1px solid ${bgDarkest}`
     }
 }));
 
+const Label = styled(Typography)(({ theme }) => ({
+    fontWeight: 700,
+    fontSize: '12px',
+    color: badgeBgLight,
+    marginBottom: '6px'
+}));
+
+const RepoSelect = styled(Select)(({ theme }) => ({
+    background: bgLight,
+    borderRadius: '2px',
+    color: paragraph,
+    fontSize: '14px'
+}));
+
+const RepoAutocomplete = styled(Autocomplete)(({ theme }) => ({
+    border: 0,
+    padding: 0,
+    '& .MuiButtonBase-root.MuiChip-root': {
+        background: bgDarkest,
+    }
+}));
+
+const Placeholder = styled(Box)(({ theme }) => ({
+    border: `3px dashed ${bgInputs}`,
+    padding: '24px',
+    textAlign: 'center',
+    color: paragraph,
+    '& .MuiButton-root': {
+        border: `1px solid ${paragraph}`,
+        color: paragraph,
+        marginTop: '0.75rem'
+    }
+}));
+
+
 export default ({ dialogOpen, handleClose }: { dialogOpen: boolean, handleClose: (open: boolean) => any }) => {
-    const classes = useStyles();
+
+    const [url, setUrl] = React.useState('Github');
+    const [branch, setBranch] = React.useState('');
+    const [type, setType] = React.useState(40);
+
+    const handleUrlChange = (event) => {
+        setUrl(event.target.value);
+    }
+
+    const handleTypeChange = (event) => {
+        setType(event.target.value);
+    }
+
+    const handleBranchChange = (event) => {
+        setBranch(event.target.value);
+    };
 
     return (
-        <Dialog
-            PaperProps={{
-                className: classes.dialog
-            }}
+        <RepoDialog
             fullWidth
             open={dialogOpen}
             onClose={handleClose}
@@ -79,15 +134,95 @@ export default ({ dialogOpen, handleClose }: { dialogOpen: boolean, handleClose:
                 </IconButton>
             </DialogTitle>
             <DialogContent>
-                <Paper className={classes.paper}>
-
-                </Paper>
+                <Box>
+                    <Label>Source</Label>
+                    <ButtonGroup fullWidth>
+                        <Select
+                            sx={{ background: bgLight, borderRadius: '2px 0px 0px 2px' }}
+                            defaultValue={url}
+                            onChange={handleUrlChange}
+                            inputProps={{ 'aria-label': 'Without label' }}
+                            IconComponent={(props) => (<ExpandMoreIcon {...props} />)}
+                        >
+                            <MenuItem value='Github'>Github</MenuItem>
+                            <MenuItem value='Gitlab'>Gitlab</MenuItem>
+                        </Select>
+                        <TextField
+                            placeholder="Repository URL"
+                            fullWidth
+                            sx = {{ border: `1px solid ${paragraph}` }}
+                        />
+                    </ButtonGroup>
+                </Box>
+                <Box>
+                    <Label>Default branch</Label>
+                    <RepoSelect
+                        fullWidth
+                        value={branch}
+                        onChange={handleBranchChange}
+                        displayEmpty
+                        inputProps={{ 'aria-label': 'Without label' }}
+                        IconComponent={(props) => (<ExpandMoreIcon {...props} />)}
+                    >
+                        <MenuItem value="">Select branch</MenuItem>
+                        <MenuItem value={10}>Main</MenuItem>
+                        <MenuItem value={20}>Develop</MenuItem>
+                        <MenuItem value={30}>Feature/628</MenuItem>
+                    </RepoSelect>
+                </Box>
+                <Box>
+                    <Label>Type</Label>
+                    <RepoSelect
+                        fullWidth
+                        onChange={handleTypeChange}
+                        defaultValue={type}
+                        displayEmpty
+                        inputProps={{ 'aria-label': 'Without label' }}
+                        IconComponent={(props) => (<ExpandMoreIcon {...props} />)}
+                    >
+                        <MenuItem value={40}>NWB Experimental data</MenuItem>
+                        <MenuItem value={50}>NWB data</MenuItem>
+                        <MenuItem value={60}>NWB</MenuItem>
+                    </RepoSelect>
+                </Box>
+                <Box>
+                    <Label>Workspace tags</Label>
+                    <RepoAutocomplete
+                        multiple
+                        options={options}
+                        defaultValue={[options[0], options[1], options[2]]}
+                        renderTags={(value, getTagProps) =>
+                            value.map((option, index) => (
+                                <Chip label={option} {...getTagProps({ index })} />
+                            ))
+                        }
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                </Box>
+                <Box>
+                    <Label>Can you describe what people can find in this repository?</Label>
+                    <TextField
+                        multiline
+                        variant="outlined"
+                        rows={5}
+                        fullWidth
+                    />
+                </Box>
+                <Box>
+                    <Label>Repository thumbnail</Label>
+                    <Placeholder>
+                        <Typography>Drop here to upload...</Typography>
+                        <Button variant="outlined">Browse files</Button>
+                    </Placeholder>
+                </Box>
             </DialogContent>
             <DialogActions>
                 <Button variant="text">Cancel</Button>
                 <Button variant="contained">Add</Button>
             </DialogActions>
 
-        </Dialog>
+        </RepoDialog>
     );
 };
+
+const options = ['testTag', 'testTag', 'testTag', 'testTag', 'testTag']
--- applications/osb-portal/src/theme.js
+++ applications/osb-portal/src/theme.js
@@ -312,11 +312,11 @@ const theme = {
         paddingTop: '10px',
         paddingBottom: '10px',
         borderRadius: inputRadius,
-        '& div:first-of-type': {
-          paddingBottom: '0px',
-        },
+        // '& div:first-of-type': {
+        //   paddingBottom: '0px',
+        // },
         '& .MuiInputBase-root': {
-          paddingTop: '0px !important',
+          // paddingTop: '0px !important',
           backgroundColor: 'transparent',
           '& .MuiAutocomplete-endAdornment': {
             display: 'none',
