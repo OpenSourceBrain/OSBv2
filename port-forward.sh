02bfe7760b6f2d723ef500f536886cdeedb2b799
Filippo Ledda <filippo@metacell.us>
2023-04-12 20:09:16 +0200

    chore: port forward script

--- port-forward.sh
+++ port-forward.sh
@@ -2,3 +2,4 @@ killall -9 kubectl
 kubectl port-forward --namespace osb2dev $(kubectl get po -n osb2dev --field-selector=status.phase==Running | grep workspaces-postgres-host | \awk '{print $1;}') 5432:5432 &
 kubectl port-forward --namespace osb2dev $(kubectl get po -n osb2dev | grep accounts | \awk '{print $1;}') 8080:8080 &
 kubectl port-forward --namespace osb2dev $(kubectl get po -n osb2dev | grep kafka | \awk '{print $1;}') 9092:9092 &
+kubectl port-forward --namespace osb2dev $(kubectl get po -n osb2dev | grep argo-osb2dev-server | \awk '{print $1;}') 2746:2746 &
