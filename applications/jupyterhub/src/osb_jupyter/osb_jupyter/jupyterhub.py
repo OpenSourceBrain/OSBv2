39199634fa77715fd47aaad3c0cbf57338159beb
Filippo Ledda <filippo@metacell.us>
2023-04-17 11:33:52 +0200

    Fix assigned pod name for anonymous

--- applications/jupyterhub/src/osb_jupyter/osb_jupyter/jupyterhub.py
+++ applications/jupyterhub/src/osb_jupyter/osb_jupyter/jupyterhub.py
@@ -113,7 +113,7 @@ def change_pod_manifest(self: KubeSpawner):
             })
     except CookieNotFound:
         # Setup a readonly default session
-        self.pod_name = f'anonymous-{self.handler.request.remote_ip.replace(".", "-")}-{appname}'
+        self.pod_name = f'anonymous-{self.user.username}-{appname}'
         from pprint import pprint
         pprint(self.volumes)
 
