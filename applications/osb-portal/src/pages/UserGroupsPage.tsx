e38e8a849516ba93f13779148349d9ab14bec839
Salam Dalloul <salam@leannode.com>
2023-05-26 13:52:55 +0200

    view real data in user groups page

--- applications/osb-portal/src/pages/UserGroupsPage.tsx
+++ applications/osb-portal/src/pages/UserGroupsPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Chip from "@mui/material/Chip";
@@ -25,9 +25,14 @@ import TableRow from "@mui/material/TableRow";
 import {TableCell} from "@mui/material";
 import Tab from "@mui/material/Tab";
 import CircularProgress from "@mui/material/CircularProgress";
+import {BitBucketIcon, GroupsIcon} from "../components/icons";
+import Tooltip from "@mui/material/Tooltip";
+import Link from "@mui/material/Link";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkIcon from "@mui/icons-material/Link";
 
 const styles = {
-  repositoriesAndWorkspaces: (theme) => ({
+  groupMembers: (theme) => ({
     flexDirection: "column",
     paddingBottom: "0px !important",
     backgroundColor: bgDarker,
@@ -40,9 +45,14 @@ const styles = {
     overflowY: 'auto',
     maxHeight: "100%",
 
+    "&.scrollbar": {
+      paddingRight: 0
+    },
+
     "&::-webkit-scrollbar": {
       display: 'none',
-      width: 0
+      width: 0,
+      paddingRight: 0
     },
 
     "& .MuiSvgIcon-root": {
@@ -126,10 +136,12 @@ function a11yProps(index: number) {
 };
 
 export const UserGroupsPage = (props: any) => {
+  const navigate = useNavigate();
   const [tabValue, setTabValue] = React.useState(0);
   const [groupMembers, setGroupMembers] = React.useState(null);
   const [groupDetails, setGroupDetails] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState<any>(null);
 
   const { groupname } = useParams<{groupname: string}>();
 
@@ -145,14 +157,21 @@ export const UserGroupsPage = (props: any) => {
     GroupsService.getGroup(groupname).then((details) => {
       setGroupDetails(details)
       GroupsService.getGroupMembers(groupname).then(users => {
-        console.log(users)
+        setGroupMembers(users)
         setLoading(false)
+        setError(null)
       })
+    }).catch(error => {
+      setError(error)
     });
   }, [groupname]);
 
-  if (loading) {
+  if (loading && !groupDetails) {
     return <CircularProgress size={24} />
+  }
+
+  if (error) {
+    throw error;
   }
   return (
     <>
@@ -169,6 +188,137 @@ export const UserGroupsPage = (props: any) => {
               <Avatar alt="group-avatar" variant="rounded" src={groupDetails?.image}>
                 {groupDetails?.name?.length > 0 && groupDetails?.name?.charAt(0)}
               </Avatar>
+              <Typography className="name" component="h1" variant="h2">
+                {groupDetails?.name}
+                {
+                  groupDetails?.email && <Typography
+                    className="username"
+                    component="p"
+                    variant="subtitle2"
+                  >
+                    {groupDetails?.email}
+                  </Typography>
+                }
+
+              </Typography>
+
+              <Typography
+                className="username"
+                component="p"
+                variant="subtitle2"
+                lineHeight={1.5}
+              >
+                {groupDetails?.description}
+              </Typography>
+
+              <Box display='flex' alignItems='center'>
+                <GroupsIcon sx={{fontSize: '0.9rem'}} />
+
+                <Typography
+                  className="username"
+                  component="p"
+                  variant="subtitle2"
+                >
+                  {groupMembers?.length} team members
+                </Typography>
+              </Box>
+
+                {groupDetails?.links && Object.keys(groupDetails?.links).length > 0 && (
+                  <Box
+                    className="links"
+                    display="flex"
+                    flexDirection="column"
+                    width="100%"
+                  >
+                    <Typography component="h2" variant="h5" gutterBottom={true}>
+                      Links
+                    </Typography>
+
+                    {groupDetails?.links?.homepage && typeof groupDetails?.links?.homepage === "string" && (
+                      <Typography
+                        component="p"
+                        variant="body2"
+                        gutterBottom={true}
+                      >
+                        <LinkIcon fontSize="small" />
+                        <Tooltip title="homepage">
+                          <Link sx={{
+                            display: "inline-block",
+                            maxWidth: "100%",
+                            overflow: "hidden",
+                            textOverflow: "ellipsis",
+                            whiteSpace: "nowrap",
+                          }}
+                                href={groupDetails?.links?.homepage} underline="hover">
+                            {groupDetails?.links?.homepage}
+                          </Link>
+                        </Tooltip>
+                      </Typography>
+                    )}
+
+                    {groupDetails?.links?.github && typeof groupDetails?.links?.github === "string" && (
+                      <Typography
+                        component="p"
+                        variant="body2"
+                        gutterBottom={true}
+                      >
+                        <GitHubIcon fontSize="small" />
+                        <Tooltip title="GitHub">
+                          <Link
+                            href={
+                              groupDetails?.links?.github.includes("github.com")
+                                ? groupDetails?.links?.github
+                                : "https://github.com/" + groupDetails?.links?.github
+                            }
+                            underline="hover"
+                          >
+                            Github profile
+                          </Link>
+                        </Tooltip>
+                      </Typography>
+                    )}
+                    {groupDetails?.links?.bitbucket && typeof groupDetails?.links?.bitbucket === "string" && (
+                      <Typography
+                        component="p"
+                        variant="body2"
+                        gutterBottom={true}
+                      >
+                        <BitBucketIcon fontSize="small" />
+                        <Tooltip title="BitBucket">
+                          <Link
+                            href={
+                              groupDetails?.links?.bitbucket.includes("bitbucket.org")
+                                ? groupDetails?.links?.bitbucket
+                                : "https://bitbucket.org/" + groupDetails?.links?.bitbucket
+                            }
+                            underline="hover"
+                          >
+                            Bitbucket profile
+                          </Link>
+                        </Tooltip>
+                      </Typography>
+                    )}
+                  </Box>
+                )}
+
+              {groupDetails?.keywords && groupDetails?.keywords > 0 && (
+                <Box className="groups" width="100%">
+                  <Typography component="h2" variant="h5" gutterBottom={true}>
+                    Groups
+                  </Typography>
+                  {groupDetails?.keywords.map((group, index) => {
+                      return (
+                        <Chip
+                          className={index === 0 ? "first-chip" : ""}
+                          color="secondary"
+                          label={group}
+                          key={group}
+                          variant="outlined"
+                        />
+                      );
+                    })}
+                </Box>
+              )}
             </Stack>
           </Grid>
 
@@ -177,7 +327,7 @@ export const UserGroupsPage = (props: any) => {
             sm={8}
             lg={9}
             className={`verticalFit`}
-            sx={styles.repositoriesAndWorkspaces}
+            sx={styles.groupMembers}
           >
             <Box
               bgcolor={bgDarkest}
@@ -199,7 +349,7 @@ export const UserGroupsPage = (props: any) => {
                       <Chip
                         size="small"
                         color="primary"
-                        label={4}
+                        label={groupMembers?.length || 0}
                       />
                     </>
                   }
@@ -207,53 +357,62 @@ export const UserGroupsPage = (props: any) => {
                 />
               </Tabs>
             </Box>
-            <Box className="scrollbar" height="100%" py={4}>
-              <TabPanel value={tabValue} index={0}>
-                <TableContainer>
-                  <Table aria-label="spanning table">
-                    <TableBody>
+            <Box className="scrollbar" height="100%" py={4} px={0}>
+              {
+                loading ? <CircularProgress size={24} /> : <TabPanel value={tabValue} index={0}>
+                  <TableContainer>
+                    <Table aria-label="spanning table">
+                      <TableBody>
+                        {
+                          groupMembers?.length !== 0 && groupMembers?.map(member => <TableRow>
+                            <TableCell component="th" scope="row">
+                              <Stack spacing={2} direction="row" alignItems="center">
+                                <Stack>
+                                  <Avatar src={member?.avatar} sx={{width: '50px', height: '50px'}}>
+                                    {(member.firstName.length > 0 && member.firstName.charAt(0)) +
+                                      (member.lastName.length > 0 && member.lastName.charAt(0))}
+                                  </Avatar>
+                                </Stack>
+                                <Stack sx={{ minWidth: 0 }}>
+                                  <Typography component="h5" variant="subtitle2" color='secondary'>
+                                    {member.firstName + " " + member.lastName}
+                                  </Typography>
+                                  <Typography
+                                    className="username"
+                                    component="p"
+                                    variant="subtitle2"
+                                  >
+                                    {member.username}
+                                  </Typography>
+                                </Stack>
+                              </Stack>
+                            </TableCell>
 
-                      <TableRow>
-                        <TableCell component="th" scope="row">
-                          <Stack spacing={2} direction="row" alignItems="center">
-                            <Stack>
-                              <Avatar sx={{width: '50px', height: '50px'}}>W</Avatar>
-                            </Stack>
-                            <Stack sx={{ minWidth: 0 }}>
-                              <Typography component="h5" variant="subtitle2" color='secondary'>
-                                Afonso Pinto
-                              </Typography>
-                              <Typography
-                                className="username"
-                                component="p"
-                                variant="subtitle2"
+                            <TableCell style={{ width: 200 }} align="right">
+                              <Button
+                                variant="outlined"
+                                color='secondary'
+                                onClick={() =>navigate(`/user/${member?.username}`)}
+                                sx={{
+                                  borderRadius: '8px',
+                                  border: '1px solid #fff',
+                                  padding: '8px 12px'
+                                }}
                               >
-                                @afonsobspinto
-                              </Typography>
-                            </Stack>
-                          </Stack>
-                        </TableCell>
+                                <Typography component="h5" variant="subtitle2" color='secondary' sx={{margin: 0}}>
+                                  See Profile
+                                </Typography>
+                              </Button>
+                            </TableCell>
+                          </TableRow>)
+                        }
 
-                        <TableCell style={{ width: 150 }} align="right">
-                          <Button
-                            variant="outlined"
-                            color='secondary'
-                            sx={{
-                              borderRadius: '8px',
-                              border: '1px solid #fff',
-                              padding: '8px 12px'
-                            }}
-                          >
-                            <Typography component="h5" variant="subtitle2" color='secondary' sx={{margin: 0}}>
-                              See Profile
-                            </Typography>
-                          </Button>
-                        </TableCell>
-                      </TableRow>
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-              </TabPanel>
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                </TabPanel>
+              }
+
             </Box>
           </Grid>
         </Grid>
