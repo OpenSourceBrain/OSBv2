8a076a9f2f4f93c6b073d3b4f2cb4b7cbd6d7f28
Filippo Ledda <filippo@metacell.us>
2023-12-04 15:43:18 +0100

    Fix jupyterhub apps parameters pass

--- applications/osb-portal/src/components/workspace/WorkspaceFrame.tsx
+++ applications/osb-portal/src/components/workspace/WorkspaceFrame.tsx
@@ -92,7 +92,7 @@ export const WorkspaceFrame = (props: {
     const type = application.subdomain.slice(0, 4);
     document.cookie = `workspaceId=${workspace.id};path=/;domain=${domain}`;
 
-    setFrameUrl(applicationDomain ? `//${applicationDomain}/hub/spawn/${userParam}/${workspace.id}${type}`: `/testapp?workspaceId=${workspace.id}&type=${type}&user=${userParam}&application=${application.name}`);
+    setFrameUrl(applicationDomain ? `//${applicationDomain}/hub/spawn/${userParam}/${workspace.id}${type}${document.location.search ?? ''}`: `/testapp?workspaceId=${workspace.id}&type=${type}&user=${userParam}&application=${application.name}`);
   }, [application]);
 
   const openResource = async () => {
