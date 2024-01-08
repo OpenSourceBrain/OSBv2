7eb17297203e230bc149f11f298d1b84fe2cf2a4
pgleeson <p.gleeson@gmail.com>
2024-01-08 13:59:15 +0000

    Initial add osb repo info

--- libraries/client/modeldb_info.py
+++ libraries/client/modeldb_info.py
@@ -24,9 +24,14 @@ gh = get_github()
 
 known_to_have_other_forks = [2730, 3343, 3658]
 
+many_forks = []
+to_be_forked = []
+errors = []
+
 if __name__ == "__main__":
     
-    max_num = 5
+    min_num = 0
+    max_num = 3700
     index = 0
 
     from osb.utils import get_page
@@ -36,7 +41,7 @@ if __name__ == "__main__":
     models = json.loads(models_json)
     pprint.pprint(models, compact=True)
 
-    selection = models[:max_num]
+    selection = models[min_num:max_num]
     for model in selection:
         print("\n--------   Model (%i/%i): %s:\n" % (index, len(selection), model))
 
@@ -51,39 +56,50 @@ if __name__ == "__main__":
 
         expected_forks = 0
         possible_mdb_repo = 'ModelDBRepository/%s'%(info[model]['id'])
-        mdb_repo = gh.get_repo(possible_mdb_repo)
-
-        repo_to_use = mdb_repo
-        print('    Exists at: %s (def branch: %s; forks: %i)'%(mdb_repo.html_url, mdb_repo.default_branch, mdb_repo.forks))
-
-        possible_osbgh_repo = 'OpenSourceBrain/%s'%(info[model]['id'])
         try:
-            osb_repo = gh.get_repo(possible_osbgh_repo)
-            print('    Exists at: %s (def branch: %s; forks: %i)'%(osb_repo.html_url, osb_repo.default_branch, osb_repo.forks))
-            repo_to_use = osb_repo
-            expected_forks+=1
+            mdb_repo = gh.get_repo(possible_mdb_repo)
+
+            repo_to_use = mdb_repo
+            print('    Exists at: %s (def branch: %s; forks: %i)'%(mdb_repo.html_url, mdb_repo.default_branch, mdb_repo.forks))
+
+            possible_osbgh_repo = 'OpenSourceBrain/%s'%(info[model]['id'])
+            try:
+                osb_repo = gh.get_repo(possible_osbgh_repo)
+                print('    Exists at: %s (def branch: %s; forks: %i)'%(osb_repo.html_url, osb_repo.default_branch, osb_repo.forks))
+                repo_to_use = osb_repo
+                expected_forks+=1
+
+                info[model]['osbv2_gh_repo'] = repo_to_use.html_url
+                info[model]['osbv2_gh_branch'] = repo_to_use.default_branch
+            except:
+                print('    Missing fork: %s, forking now: %s'%(possible_osbgh_repo, fork_if_missing))
+                if fork_if_missing:
+                    print('    Forking to: %s...'%possible_osbgh_repo)
+                    org = gh.get_organization('OpenSourceBrain')
+                    org.create_fork(mdb_repo,default_branch_only=False)
+                    print('    Forked!')
+                else:
+                    msg = '    Yet to be forked: %i %s'%(info[model]['id'],info[model]['name'])
+                    print(msg)
+                    to_be_forked.append(msg)
+
+
+            if (not mdb_repo.forks==expected_forks) and (not (info[model]['id'] in known_to_have_other_forks)):
+                many_forks.append('Unexpected forks for %i (%s != %s)...'%(info[model]['id'], mdb_repo.forks,expected_forks))
+
         except:
-            print('    Missing fork: %s, forking now: %s'%(possible_osbgh_repo, fork_if_missing))
-            if fork_if_missing:
-                print('    Forking to: %s...'%possible_osbgh_repo)
-                org = gh.get_organization('OpenSourceBrain')
-                org.create_fork(mdb_repo,default_branch_only=False)
-                print('    Forked!')
-
-        if (not mdb_repo.forks==expected_forks) and (not (info[model]['id'] in known_to_have_other_forks)):
-            print('Unexpected forks for %i (%s != %s)...'%(info[model]['id'], mdb_repo.forks,expected_forks))
+            msg = '    Problem locating repo for: %i %s'%(info[model]['id'],info[model]['name'])
+            print(msg)
+            errors.append(msg)
     
-            exit()
-
-        info[model]['osbv2_gh_repo'] = repo_to_use.html_url
-        info[model]['osbv2_gh_branch'] = repo_to_use.default_branch
+            
 
         index +=1
 
     if verbose:
         infop = pprint.pprint(info, compact=True)
 
-    print("\nThere were %i models, %i of which had gitrepo\n"%(len(info), with_gitrepo))
+    print("\nThere were %i models checked\n"%(len(info)))
 
 filename = 'cached_info/modeldb.json'  
 
@@ -91,5 +107,19 @@ strj = json.dumps(info, indent='    ', sort_keys=True)
 with open(filename, "w") as fp:
     fp.write(strj)
 
+print("\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"+
+      "\n\nDone!")
+ 
+print("\nStill to be forked(%i total):"%len(to_be_forked))     
+for m in to_be_forked:
+    print(m)
+
+print("\nMany forks (%i total):"%len(many_forks))  
+for m in many_forks:
+    print(m)
+
+print("\nErrors (%i total):"%len(errors))  
+for m in errors:
+    print(m)
 
 print('Data on ModelDB (%i models) written to %s'%(len(info),filename))
