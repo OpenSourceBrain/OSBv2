3f18cf541833accaf71bae73df5bb37fef1fb079
Filippo Ledda <filippo@metacell.us>
2024-01-10 19:35:53 +0100

    #874 improve logging

--- applications/workspaces/tasks/github-copy/README.md
+++ applications/workspaces/tasks/github-copy/README.md
@@ -4,6 +4,6 @@
 How to test
 
 ```
-shared_directory=/tmp folder=osbv2/develop url=https://github.com/OpenSourceBrain/OSBv2 branch=develop paths="`applications/workspaces` `README.md`" ./run.sh
+shared_directory=/tmp folder=osbv2/develop url=https://github.com/OpenSourceBrain/OSBv2 branch=develop paths="'applications/workspaces' 'README.md'" ./run.sh
 ```
 
--- applications/workspaces/tasks/github-copy/run.sh
+++ applications/workspaces/tasks/github-copy/run.sh
@@ -9,17 +9,18 @@ export download_path=`echo $shared_directory | cut -d ":" -f 2`/"${folder}"
 mkdir -p "${download_path}"
 cd "${download_path}"
 
-
-
 echo GitHub copy "$paths" to "$download_path"
 ls -la
+echo "Fix permissions"
 git config --global --add safe.directory "$download_path"
 # do the next command only if download path does not exist
 if [ ! -d "${download_path}/.git" ]; then
+  echo Cloning $branch
   git clone -n "${url}" --branch $branch "${download_path}"
 fi
-
+echo "Checking out $paths"
 git checkout HEAD ${paths}
 
 # fix permissions
 chown -R 1000:1000 "${download_path}"
+ls -la
