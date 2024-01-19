52c099690aa0a18f02baa7420bbc1b814aa2592b
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2024-01-19 11:57:59 +0000

    fix(github-copy): unset git user/email instead of leaving blank
    If it's unset, it will read the value from global git config, but if
    it's blank, it won't. If the user does have a global git config, we want
    that to work.

--- applications/workspaces/tasks/github-copy/run.sh
+++ applications/workspaces/tasks/github-copy/run.sh
@@ -14,19 +14,17 @@ cd "${download_path}"
 
 echo "GitHub copy $paths to $download_path"
 ls -la
+
 echo "Git: fix permissions"
 git config --global --add safe.directory "$download_path"
-# set username for commit
-gituser=$(git config --list | grep user.name)
-gitusermail=$(git config --list | grep user.email)
-
 
 # do the next command only if download path does not exist (initial clone)
 if [ ! -d "${download_path}/.git" ]; then
-  echo "Git: cloning $branch"
+  echo "Git: cloning new repository branch: $branch"
   git clone -n "${url}" --branch $branch "${download_path}"
 
-  # configure OSB user
+  # configure OSB user for this clone
+  echo "Git: setting user/email to OSBv2 for new repo"
   git config user.name "OSBv2"
   git config user.email "info@opensourcebrain.org"
 
@@ -44,28 +42,31 @@ if [ ! -d "${download_path}/.git" ]; then
   fi
 
   # create new branch, commit there
+  echo "Git: checking out new branch and committing"
   git checkout -b "$timestamp"
   git commit -m "osbv2: checked out repository"
 
   # unset username
-  git config user.name ""
-  git config user.email ""
+  echo "Git: unsetting user/email"
+  git config --unset user.name
+  git config --unset user.email
 
 # on subsequent updates from same repo
 else
   echo "Updating existing repository"
   # configure OSB user
-  gituser="$(git config user.name)"
+  echo "Git: saving current git user/email and setting to OSBv2"
+  gituser="$(git config user.name)" || true
   git config user.name "OSBv2"
-  gitemail="$(git config user.email)"
+  gitemail="$(git config user.email)" || true
   git config user.email "info@opensourcebrain.org"
 
-  # save state, but do not exit if there's nothing to save
-  echo "Git: saving state"
+  # save state, but do not error if there's nothing to save
+  echo "Git: saving current state"
   git commit -a -m "osbv2: saving state at $timestamp" || true
 
   # do not assume user has left the default remote name as "origin"
-  echo "Git: adding remote"
+  echo "Git: adding and fetching from temporary remote"
   git remote add "$timestamp" "${url}"
   git fetch "$timestamp" "$branch"
 
@@ -80,17 +81,21 @@ else
           git checkout $timestamp/$branch -- "$path"
       done
   fi
-  git commit -a -m "osbv2: updated state at $timestamp"
+  # save new state but do not error if nothing changed
+  echo "Git: saving new state"
+  git commit -a -m "osbv2: updated state at $timestamp" || true
 
-  echo "Git: undoing remote/user"
+  echo "Git: removing temporary remote"
   # remove temporary remote
   git remote remove "$timestamp"
   # reset user
+  echo "Git: resetting user/email"
   git config user.name "$gituser"
   git config user.email "$gitemail"
 fi
 
 
 # fix permissions
+echo "Fixing permissions on download path"
 chown -R 1000:100 "${download_path}"
 ls -la
