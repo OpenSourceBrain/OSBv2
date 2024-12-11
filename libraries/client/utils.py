718b85e5a08fe23c98246a15b48b6460ee509976
pgleeson <p.gleeson@gmail.com>
2024-12-11 15:17:28 +0000

    Regenerated multiple caches & updated osbv2 live

--- libraries/client/utils.py
+++ libraries/client/utils.py
@@ -4,6 +4,7 @@ known_users = {'Padraig_v2':"0103eaaf-6a34-4509-a025-14367a52aa2b",
                'Filippo_v2': '1839ee9a-d481-4261-b7dc-b282c34e0ac6',
                'Ankur_v2': '3ad0ac48-617e-4b3e-ad5e-ace059cc0a07',
                'OSBAdmin_v2': '7aafb661-2f39-4683-8f35-528de0752dd7',
+               'Sietse_v2': '00711324-c645-4a23-8f4b-6c54bde46a2f',
 
                'Padraig_v2dev': "7089f659-90ad-4ed9-9715-2327f7e2e72f",
                'Filippo_v2dev': 'a2514035-c47f-4d8a-b22b-081d91a5ce6b',
