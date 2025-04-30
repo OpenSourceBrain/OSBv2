32996a654aca70fbd09d40a9f5a9ca4486642cd3
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2025-04-30 12:17:30 +0100

    chore(ebrains): register adapter

--- applications/workspaces/server/workspaces/service/osbrepository/adapters/__init__.py
+++ applications/workspaces/server/workspaces/service/osbrepository/adapters/__init__.py
@@ -2,3 +2,4 @@ from workspaces.service.osbrepository.adapters.dandiadapter import DandiAdapter
 from workspaces.service.osbrepository.adapters.figshareadapter import FigShareAdapter
 from workspaces.service.osbrepository.adapters.githubadapter import GitHubAdapter
 from workspaces.service.osbrepository.adapters.biomodelsadapter import BiomodelsAdapter
+from workspaces.service.osbrepository.adapters.ebrainsadapter import EbrainsAdapter
