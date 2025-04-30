e28d361f491839cb41c40660bd2fb08cc825f9a8
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2025-04-30 14:04:31 +0100

    feat(ebrains): implement getting resources in adapter

--- applications/workspaces/server/workspaces/service/osbrepository/adapters/ebrainsadapter.py
+++ applications/workspaces/server/workspaces/service/osbrepository/adapters/ebrainsadapter.py
@@ -1,7 +1,8 @@
 import re
 import sys
-from typing import List
+from typing import List, Optional
 import requests
+from functools import cache, cached_property
 
 
 from fairgraph import KGClient, KGProxy
@@ -13,6 +14,7 @@ from workspaces.models.resource_origin import ResourceOrigin
 from workspaces.models.biomodels_repository_resource import EbrainsRepositoryResource
 
 from .utils import add_to_tree
+from .githubadapter import GitHubAdapter
 
 
 class EbrainsException(Exception):
@@ -31,6 +33,7 @@ class EbrainsAdapter:
         self.uri = uri if uri else osbrepository.uri
         self.api_url = "https://search.kg.ebrains.eu/"
         # TODO: get permanent application auth token from EBRAINS
+        # self.kg_client = KGClient(token="", host="core.kg.ebrains.eu")
         self.kg_client = KGClient(client_id="SOME ID", client_secret="SOME SECRET", host="core.kg.ebrains.eu")
 
         try:
@@ -41,37 +44,291 @@ class EbrainsAdapter:
         except AttributeError:
             raise EbrainsException(f"{uri} is not a valid Ebrains URL")
 
-    def get_json(self, uri=None):
+    @cache
+    def get_model(self, uri: Optional[str] = None) -> Model:
+        """Get model object using FairGraph"""
         logger.debug(f"Getting: {self.model_id}")
+        model = Model.from_id(id=self.model_id, client=self.kg_client)
 
-        model = Model.from_id(id=self.model_id, client=self.client)
         return model
-
 
     def get_base_uri(self):
         return self.uri
 
+    @cache
     def get_info(self) -> RepositoryInfo:
-        info = self.get_json()
-        return RepositoryInfo(name=info["name"], contexts=self.get_contexts(), tags=info["format"]["name"], summary=info.get("description", ""))
+        """Get repository metadata from model object"""
+        model = self.get_model()
+        return RepositoryInfo(name=model.name, contexts=self.get_contexts(), tags=self._get_keywords(model), summary=model.description)
 
-    def get_contexts(self):
-        result = self.get_json()
-        if isinstance(result.versions, list):
-            revisions = result.versions
+    def _get_keywords(self, model: Model) -> list[str]:
+        """Get keywords from model
+
+        :param model: model object
+        :returns: list of keywords
+
+        """
+        keywords: list[str] = []
+        if model.study_targets:
+            if isinstance(model.study_targets, KGProxy):
+                keyws = model.study_targets.resolve(self.kg_client)
+            else:
+                keyws = model.study_targets
+
+            if isinstance(keyws, list):
+                for k in keyws:
+                    if isinstance(k, KGProxy):
+                        try:
+                            keywords.append(k.resolve(self.kg_client).name)
+                        except ResolutionFailure:
+                            pass
+                    else:
+                        keywords.append(k.name)
+            else:
+                if isinstance(keyws, KGProxy):
+                    keywords.append(keyws.resolve().name)
+                else:
+                    keywords.append(keyws.name)
+
+        if model.abstraction_level:
+            if isinstance(model.abstraction_level, KGProxy):
+                abs_l = model.abstraction_level.resolve(self.kg_client)
+            else:
+                abs_l = model.abstraction_level
+            keywords.append(abs_l.name)
+
+        return keywords
+
+    @cache
+    def get_contexts(self) -> list[str]:
+        model = self.get_model()
+        if isinstance(model.versions, list):
+            versions = model.versions
         else:
-            revisions = [result.versions]
-        return revisions
+            versions = [model.versions]
 
-    def _get_filelist(self, context):
-        logger.debug(f"Getting filelist: {context}")
-        contents = self.get_json()
-        files = (contents.get("additional", []) + contents.get("main", []))
-        return files
+        contexts = []
+
+        for v in versions:
+            v_r: Optional[ModelVersion] = None
+            if isinstance(v, KGProxy):
+                try:
+                    v_r = v.resolve(self.kg_client)
+                except ResolutionFailure:
+                    logger.error(f"ERROR: Could not resolve {v.id}")
+                    continue
+            else:
+                v_r = v
+
+            if v_r:
+                contexts.append(v_r.version_identifier)
+
+        return contexts
+
+    def _get_file_storage_url(self, context: str) -> Optional[str]:
+        """Get the URL of the file storage for the provided context
+
+        :param context: TODO
+        :returns: TODO
+
+        """
+        model = self.get_model()
+        if isinstance(model.versions, list):
+            versions = model.versions
+        else:
+            versions = [model.versions]
+
+        for v in versions:
+            v_r: Optional[ModelVersion] = None
+            if isinstance(v, KGProxy):
+                try:
+                    v_r = v.resolve(self.kg_client)
+                except ResolutionFailure:
+                    logger.error(f"ERROR: Could not resolve {v.id}")
+                    continue
+            else:
+                v_r = v
+            if v_r:
+                if context == v_r.version_identifier:
+                    repository: FileRepository = v_r.repository
+                    try:
+                        repository_r = repository.resolve(self.kg_client)
+                        return repository_r.name
+                    except ResolutionFailure:
+                        logger.error(f"Could not resolve {repository.id}")
+
+        return None
+
+
+    def _get_ebrains_data_proxy_file_list(self, url: str) -> list[str]:
+        """Get the list of files from an ebrains data proxy URL.
+
+        The complete url will be of this form:
+
+        .. code-block::
+
+            https://data-proxy.ebrains.eu/api/v1/buckets/m-0ffae3c2-443c-44fd-919f-70a4b01506a4?prefix=CA1_pyr_mpg150211_A_idA_080220241322/
+
+        The API documentation is here:
+        https://data-proxy.ebrains.eu/api/docs
+
+        This URL returns a JSON response with all the objects listed.
+        So we can get the file list from there. To get the download URL, we need
+        this end point for each object in the list:
+
+        .. code-block::
+
+            /v1/buckets/{bucket_name}/{object_name}
+
+        :param url: url of repository
+        :returns: dict of files and their download URLs
+
+        """
+        file_list: dict[str, str] = {}
+        top_level_url: str = url.split("?prefix=")[0]
+
+        r = requests.get(url)
+        if r.status_code == 200:
+            logger.debug("data-proxy: response is")
+            logger.debug(r)
+
+            json_r = r.json()
+            object_list = json_r["objects"]
+            for anobject in object_list:
+                object_url = top_level_url + "/" + anobject["name"]
+                file_list[anobject["name"]] = object_url
+
+        else:
+            logger.error(f"Something went wrong: {r.response_code}")
+
+        if len(file_list.items()) == 0:
+            logger.warn("No files found for this: check kg.ebrains.eu to verify")
+
+        return file_list
+
+
+    def _get_cscs_file_list(self, url: str) -> dict[str, str]:
+        """Get the list of files from a CSCS repository URL.
+
+        The complete url will be of this form:
+
+        .. code-block:
+
+            https://object.cscs.ch/v1/AUTH_c0a333ecf7c045809321ce9d9ecdfdea/hippocampus_optimization/rat/CA1/v4.0.5/optimizations_Python3/CA1_pyr_cACpyr_mpg141208_B_idA_20190328144006/CA1_pyr_cACpyr_mpg141208_B_idA_20190328144006.zip?use_cell=cell_seed3_0.hoc&bluenaas=true
+
+        To get the file list, we only need the top level:
+
+        .. code-block::
+
+            https://object.cscs.ch/v1/AUTH_c0a333ecf7c045809321ce9d9ecdfdea/hippocampus_optimization
+
+        We then need to limit the file list to the bits we want, because the top
+        level container contains all the files and all the versions:
+
+        .. code-block::
+
+            rat/CA1/v4.0.5/optimizations_Python3/CA1_pyr_cACpyr_mpg141208_B_idA_20190328144006/CA1_pyr_cACpyr_mpg141208_B_idA_20190328144006
+
+        Note that even if the url is wrong (eg, in the shown example, the file list
+        does not include a folder called `optimizations_Python3` at all), the cscs
+        server still returns a zip. However, manually checking search.kg.ebrains.eu
+        shows that the corresponding entry does not have a file list. It simply
+        says "no files available".
+
+        Also note that the url may include a `prefix=` parameter which specifies
+        the file directory structure.
+
+        Most of these directories also include a zipped version. For the moment, we
+        include this in the file list.
+
+        :param url: url of repository
+        :returns: dict of files and their download URLs
+
+        """
+        file_list: dict[str, str] = {}
+        file_list_url: str = ""
+        file_list_string: str = ""
+
+        special_suffixes = [
+            "py",
+            "hoc",
+            "xz",
+            "zip",
+            "pkl",
+            "json",
+            "pdf",
+            "mod",
+            "txt",
+            "png",
+            "zip",
+            "ipynb",
+        ]
+
+        logger.debug(f"Getting file list for {url}")
+        if ".zip?" in url:
+            url_portions: list[str] = url.split(".zip")[0].split("/")
+            file_list_url = "/".join(url_portions[:6])
+            file_list_string = "/".join(url_portions[6:])
+        # assume it's with prefix
+        elif "?prefix=" in url:
+            file_list_url = url.split("?prefix=")[0]
+            file_list_string = url.split("?prefix=")[1]
+        else:
+            logger.warning(f"Other cscs url format: {url}")
+            # test if it's one of the file types
+            for suf in special_suffixes:
+                if url.endswith(suf):
+                    file_list = {url: url}
+                    break
+
+            return file_list
+
+        r = requests.get(file_list_url)
+        if r.status_code == 200:
+            for line in r.text.split():
+                if (
+                    line.startswith(file_list_string)
+                    and not line.endswith("/")
+                    and line != file_list_string
+                ):
+                    file_list[line] = file_list_url + "/" + line
+        else:
+            logger.error(f"Something went wrong: {r.response_code}")
+
+        if len(file_list) == 0:
+            logger.warn("No files found for this: check kg.ebrains.eu to verify")
+        return file_list
+
 
     def get_resources(self, context):
         logger.debug(f"Getting resources: {context}")
-        files = self._get_filelist(context)
+
+        download_url = self._get_file_storage_url(context)
+        if "github" in download_url:
+            gh_adapter = GitHubAdapter(self.osbrepository, download_url)
+            return gh_adapter.get_resources(context)
+
+        elif "modeldb" in download_url.lower():
+            model_id = ""
+            # urls with model id after the last slash
+            # https://modeldb.science/249408?tab=7
+            if "modeldb.science" in download_url or "modeldb.yale.edu" in download_url:
+                model_id = download_url.split("/")[-1].split('?')[0]
+            # legacy urls with model id as a parameter
+            # https://senselab.med.yale.edu/ModelDB/showmodel.cshtml?model=249408#tabs-1
+            else:
+                model_id = download_url.split("?model=")[-1].split('#')[0]
+
+            modeldb_url = f"https://github.com/ModelDBRepository/{model_id}"
+            gh_adapter = GitHubAdapter(self.osbrepository, modeldb_url)
+            return gh_adapter.get_resources(context)
+
+        elif "cscs.ch" in download_url:
+            files = self._get_cscs_file_list(download_url)
+        elif "data-proxy.ebrains.eu" in download_url:
+            files = self._get_ebrains_data_proxy_file_list(download_url)
+        else:
+            files = ["TODO: handle other special cases"]
 
         tree = RepositoryResourceNode(
             resource=EbrainsRepositoryResource(
@@ -83,13 +340,11 @@ class EbrainsAdapter:
             children=[],
         )
 
-        for afile in files:
-            download_url = f"{self.api_url}/model/download/{self.model_id}.{context}?filename={afile['name']}"
+        for afile, url in files.items():
             add_to_tree(
                 tree=tree,
-                tree_path=[afile["name"]],
-                path=download_url,
-                size=int(afile["fileSize"]),
+                tree_path=afile,
+                path=url,
                 osbrepository_id=self.osbrepository.id,
             )
 
@@ -98,8 +353,8 @@ class EbrainsAdapter:
     def get_description(self, context):
         logger.debug(f"Getting description: {context}")
         try:
-            result = self.get_json()
-            return result["description"]
+            result = self.get_model()
+            return result.description
         except Exception as e:
             logger.debug(
                 "unable to get the description from biomodels, %", str(e))
@@ -109,10 +364,9 @@ class EbrainsAdapter:
         # using the format name for the moment, since they don't do explict
         # tags/keywords
         logger.debug(f"Getting tags: {context}")
-        result = self.get_json()
-        return result["format"]["name"]
+        model = self.get_model()
+        return self._get_keywords(model)
 
-    # biomodels files are usually small, so one task is enough
     def create_copy_task(self, workspace_id, origins: List[ResourceOrigin]):
         import workspaces.service.workflow as workflow
 
