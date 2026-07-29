3ebaef362e4ca2243121b5e076324bc773797265
Padraig Gleeson <p.gleeson@gmail.com>
2026-07-29 14:28:11 +0100

    Allow nwb docker build to run on mac

--- applications/nwb-explorer/build_local.sh
+++ applications/nwb-explorer/build_local.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -e
 
-time DOCKER_BUILDKIT=1 docker build -t mynwbosb -f Dockerfile .
+# Set the platform flag if we're on ARM
+arch=$(uname -m)
+if [[ "$arch" == "arm64" || "$arch" == "aarch64" ]]; then
+    platform_flag="--platform linux/amd64"
+else
+    platform_flag=""
+fi
+
+time DOCKER_BUILDKIT=1 docker build $platform_flag -t mynwbosb -f Dockerfile .
